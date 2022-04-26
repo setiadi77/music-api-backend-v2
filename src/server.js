0a640b84318489aff882804eca741ce5567890f6
Setiadi <34624281+setiadi77@users.noreply.github.com>
2022-04-27 03:37:32 +0700

    autentikasi
    migrasi db auth, user
    update package.json
    plugin api authentications
    plugin api user
    exceptions authservice
    usersservices
    toknize token manager
    validator authentications
    validator users

--- src/server.js
+++ src/server.js
@@ -1,18 +1,35 @@
 require('dotenv').config();
 
+const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require("./exceptions/ClientError");
 
-const Hapi = require('@hapi/hapi');
+//albums
 const albums = require('./api/albums');
-const songs = require('./api/songs');
 const AlbumsService = require('./services/AlbumsService');
-const SongsService = require('./services/SongsService');
 const AlbumsValidator = require('./validator/albums');
+
+//songs
+const songs = require('./api/songs');
+const SongsService = require('./services/SongsService');
 const SongsValidator = require('./validator/songs');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/UsersService');
+const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
     const albumsService = new AlbumsService();
     const songService = new SongsService();
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -23,7 +40,30 @@ const init = async () => {
             },
         },
     });
-            //saran
+
+    // await server.register([
+    //     {
+    //         plugin: Jwt,
+    //     },
+    // ]);
+
+    // server.auth.strategy('musicapp_jwt', 'jwt', {
+    //     keys: process.env.ACCESS_TOKEN_KEY,
+    //     verify: {
+    //         aud: false,
+    //         iss: false,
+    //         sub: false,
+    //         maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    //     },
+    //     validate: (artifacts) => ({
+    //         isValid: true,
+    //         credentials: {
+    //             id: artifacts.decoded.payload.id,
+    //         },
+    //     }),
+    // });
+
+    //saran
     await server.register([
         {
             plugin: albums,
@@ -38,15 +78,29 @@ const init = async () => {
                 service: songService,
                 validator: SongsValidator,
             },
-        }
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
     ])
-
+    //saran
     server.ext('onPreResponse', (request, h) => {
-        // mendapatkan konteks response dari request
         const { response } = request;
 
         if (response instanceof ClientError) {
-            // membuat response baru dari response toolkit sesuai kebutuhan error handling
             const newResponse = h.response({
                 status: 'fail',
                 message: response.message,
@@ -55,7 +109,6 @@ const init = async () => {
             return newResponse;
         }
 
-        // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
         return response.continue || response;
     });
 
